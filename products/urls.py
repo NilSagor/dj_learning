c4bdb0e722d565fe40bab913cad326007be85b26
NilSagor <masum1201@gmail.com>
2019-05-10 17:04:56 +0800

    product models added

--- products/urls.py
+++ products/urls.py
@@ -13,24 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 from django.contrib import admin
 from django.urls import path, include
 
-from . import views
+# from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.HomePageView.as_view(), name = 'home'),
-    path('contact/', views.ContactPageView.as_view(), name = 'contact'),
-    path('login/', views.LoginPageView, name = 'login'),
-    path('register/', views.RegisterPageView, name = 'register'),
-    path('products/', include('products.urls')),
+    # path('admin/', admin.site.urls),
+  
 ]
 
 
-if settings.DEBUG:
-	urlpatterns  +=  (static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
-	urlpatterns  +=  (static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
