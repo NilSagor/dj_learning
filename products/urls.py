b3220e5103de416c28550c630b09e2f6e0f49930
NilSagor <masum1201@gmail.com>
2019-05-10 17:47:25 +0800

    image model added

--- products/urls.py
+++ products/urls.py
@@ -18,11 +18,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-# from . import views
+from . import views
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-  
+    path('', views.ProductListView.as_view(), name = 'product_list'),
+    path('<int:pk>/', views.ProductDetailView.as_view(), name = 'product_detail'),
 ]
 
 
--- products/views.py
+++ products/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView 
+
+from .models import Product
 
 # Create your views here.
+class ProductListView(ListView):
+	queryset = Product.objects.all()
+	template_name = 'products/product_list.html'
+
+	def get_context_data(self, *args, **kwargs):
+		context = super(ProductListView, self).get_context_data(*args, **kwargs)
+		return context
+
+class ProductDetailView(DetailView):
+	queryset = Product.objects.all()
+	template_name = 'products/product_detail.html'
+
+	def get_context_data(self, *args, **kwargs):
+		context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+		return context
